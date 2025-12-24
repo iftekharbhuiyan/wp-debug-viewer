e2554c2ee9ba16fd9d43d4440889068ab6d07643
iftekhar bhuiyan <ibhuiyan@mail.com>
2025-12-24 14:12:37 +0600

    Code update

--- Readme.txt
+++ Readme.txt
@@ -1,8 +1,7 @@
 === Debug Viewer ===
 Plugin Name: Debug Viewer
-Plugin URI: https://github.com/iftekharbhuiyan/wp-debug-viewer
-Tags: debug log, debug viewer, site administrator
-Author URI: https://github.com/iftekharbhuiyan/
+Tags: debug log, debug viewer, administrator
+Author URI: https://profiles.wordpress.org/iftekharbhuiyan/
 Author: Iftekhar Bhuiyan
 Requires at least: 5.0
 Requires PHP: 8.0
@@ -11,16 +10,16 @@ Stable tag: 1.0.0
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 
-This plugin allows site administrators to view the debug log from the Dashboard.
+A Plugin that allows site Administrators to see debug log content from Dashboard.
 
 == Description ==
 
-This plugin allows site administrators to view the debug log from the Dashboard.
+A Plugin that allows site Administrators to see debug log content from Dashboard.
 
 
 == Installation ==
 
-1. Upload the `wp-debug-viewer` folder to your site's `/wp-content/plugins/` directory.
+1. Upload the `debug-viewer` folder to your site's `/wp-content/plugins/` directory.
 2. Activate the plugin from the Plugins screen in WordPress.
 
 
--- debug-viewer.php
+++ debug-viewer.php
@@ -1,15 +1,13 @@
 <?php
 /**
  * Plugin Name: Debug Viewer
- * Plugin URI: https://github.com/iftekharbhuiyan/wp-debug-viewer
- * Description: This plugin allows site Administrators to see debug log content from Dashboard.
+ * Description: A Plugin that allows site Administrators to see debug log content from Dashboard.
  * Author: Iftekhar Bhuiyan
  * Version: 1.0.0
  * Requires PHP: 8.0
- * Author URI: https://github.com/iftekharbhuiyan/
+ * Author URI: https://profiles.wordpress.org/iftekharbhuiyan/
  * License: GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
- * Update URI: https://github.com/iftekharbhuiyan/wp-debug-viewer
  */
 
 // disable direct loading
@@ -17,19 +15,27 @@ if (!defined('ABSPATH')) {
     die('Invalid request.');
 }
 
+// check class existence
 if (!class_exists('BSFT_Debug_Viewer')) :
 
     class BSFT_Debug_Viewer {
 
-        private function __construct() {}
+        private $class_file;
+        private $debug_file;
 
-        // add widget
-        public static function hcb_add_dashboard_widget() {
+        public function __construct() {
+            $this->class_file = dirname(__FILE__) . '/debug-viewer.php';
+            $this->debug_file = (defined('WP_CONTENT_DIR') ? WP_CONTENT_DIR : ABSPATH . 'wp-content') . '/debug.log';
+        }
+
+        // add widget callback
+        public function add_widget() {
             if (is_admin() && current_user_can('manage_options')) {
+                // dashboard widget
                 wp_add_dashboard_widget(
                     'debug_viewer_widget',
-                    'Debug Log',
-                    array(__CLASS__, 'hcb_set_viewer'),
+                    'Debug Viewer',
+                    array($this, 'get_viewer'),
                     null,
                     null,
                     'side',
@@ -39,33 +45,28 @@ if (!class_exists('BSFT_Debug_Viewer')) :
         }
 
         // render viewer
-        public static function hcb_set_viewer() {
-            // get the file location
-            $debug_file = (defined('WP_CONTENT_DIR') ? WP_CONTENT_DIR : ABSPATH . 'wp-content') . '/debug.log';
+        public function get_viewer() {
             $file_mb_format = '0.00';
-            // form check for nonce and capability
+            // form submission
             if (isset($_POST['debug_nonce']) && wp_verify_nonce($_POST['debug_nonce'], basename(__FILE__))) {
                 if (current_user_can('manage_options')) {
-                    if (file_exists($debug_file)) {
+                    if (file_exists($this->debug_file)) {
                         if (!function_exists('wp_delete_file')) {
                             require_once ABSPATH . 'wp-admin/includes/file.php';
                         }
-                        wp_delete_file($debug_file);
+                        wp_delete_file($this->debug_file);
                     }
-                    wp_safe_redirect(esc_url_raw(admin_url('index.php')));
-                    exit;
                 }
             }
             // rendering content
             $data = '<div class="main">';
-            $data .= '<textarea id="debugLog" class="large-text" style="width: 100%; height: 15em;" readonly>';
+            $data .= '<textarea class="small-text" style="width: 100%; height: 15em;" readonly>';
             // read the log data
-            if (file_exists($debug_file)) {
-                // calculate file size
-                $file_size = filesize($debug_file);
+            if (file_exists($this->debug_file)) {
+                $file_size = filesize($this->debug_file);
                 $file_mb = ($file_size / (1024 * 1024));
-                $file_mb_format = number_format($file_mb, 2) . ' MB';
-                $data .= esc_textarea(file_get_contents($debug_file));
+                $file_mb_format = number_format($file_mb, 2);
+                $data .= esc_textarea(file_get_contents($this->debug_file));
             } else {
                 $data .= esc_html('File does not exist!');
             }
@@ -73,41 +74,48 @@ if (!class_exists('BSFT_Debug_Viewer')) :
             $data .= '<div style="display: flex; margin: 10px -12px -12px -12px; padding: 10px; ';
             $data .= 'border-top: 1px solid #dcdcde; background-color: #f6f7f7;">';
             $data .= '<div style="flex: 1 1 50%;">';
-            if (file_exists($debug_file)) :
+            // delete button
+            if (file_exists($this->debug_file)) :
                 $data .= '<form action="' . esc_url(admin_url('index.php')) . '" method="post">';
-                $data .= '<input type="hidden" id="debug_nonce" name="debug_nonce" value="' . esc_attr(wp_create_nonce(basename(__FILE__))) . '">';
-                $data .= '<button type="submit" class="button button-primary">' . esc_html('Delete Log') . '</button>';
+                $data .= '<input type="hidden" id="debug_nonce" name="debug_nonce" value="'.esc_attr(wp_create_nonce(basename(__FILE__))).'">';
+                $data .= '<button type="submit" class="button button-primary">'.esc_html('Delete Log').'</button>';
                 $data .= '</form>';
             endif;
             $data .= '</div>';
-            $data .= '<div style="flex: 1 1 50%; align-self: center; text-align: right; ">';
+            $data .= '<div style="flex: 1 1 50%; align-self: center; text-align: right;">';
             $data .= '<strong>'.$file_mb_format.' MB</strong></div>';
             $data .= '</div>';
             $data .= '</div>';
             echo $data;
         }
 
-        // activate
-        public static function activate() {
+        // activate callback
+        public function activate() {
             return;
         }
-        // deactivate
-        public static function deactivate() {
+
+        // deactivate callback
+        public function deactivate() {
             return;
         }
 
         // init
-        public static function init() {
-            // run on activation
-            register_activation_hook(__FILE__, array(__CLASS__, 'activate'));
-            // run on deactivation
-            register_deactivation_hook(__FILE__, array(__CLASS__, 'deactivate'));
+        public function init() {
+            // on activation
+            register_activation_hook($this->class_file, array($this, 'activate'));
+
+            // on deactivation
+            register_deactivation_hook($this->class_file, array($this, 'deactivate'));
+
             // add dashboard widget
-            add_action('wp_dashboard_setup', array(__CLASS__, 'hcb_add_dashboard_widget'));
+            add_action('wp_dashboard_setup', array($this, 'add_widget'));
         }
     }
 
+    // instance
+    $bsft_debug_viewer = new BSFT_Debug_Viewer();
+
     // loaded
-    add_action('plugins_loaded', array('BSFT_Debug_Viewer', 'init'));
+    add_action('plugins_loaded', array($bsft_debug_viewer, 'init'));
 
 endif;
